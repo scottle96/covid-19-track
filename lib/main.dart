f8cb74d7bcb67ae5298a509b20b3fcb3116b7408
unknown <scottyleclair@rocketmail.com>
2020-12-16 08:56:07 -0500

    66% terminé

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(App());
-}
+void main() => runApp(MyApp());
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
-      title: 'Flutter Navigation',
-      home: PageAccueil(),
+      home: Scaffold(
+        body: PageAccueil(),
+      ),
     );
   }
 }
 
-// Page d'accueil
-
-class PageAccueil extends StatelessWidget {
-  Future naviguerPageUn(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageUn()));
+// Create a Form widget.
+class PageAccueil extends StatefulWidget {
+  @override
+  PageAccueilState createState() {
+    return PageAccueilState();
   }
-  
-  Future naviguerPageDeux(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageDeux()));
-  }
+}
 
-  Future naviguerPageTrois(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageTrois()));
-  }
+// Create a corresponding State class.
+// This class holds data related to the form.
+class PageAccueilState extends State<PageAccueil> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+  var texteTemp = "";
 
-  Future naviguerPageAdieu(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageAdieu()));
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page d''accueil'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-                  'images/logo.jpg',
-                  width:270,
-                  height:60,
-                  fit:BoxFit.fill,
-                ),
-            Container(height:150),
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(height:150),
+          Container(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                    child:                  
+                      Image.asset(
+                        'images/logo.jpg',
+                        width:300,
+                        height:60,
+                        fit:BoxFit.contain,
+                        //alignment: Alignment.center,
+                      ),
+                )
+              ),
+            Container(height:120),
             Text('Bienvenue!', style: TextStyle(fontSize:16)
             ),
             Container(height:10),
@@ -57,116 +72,165 @@ class PageAccueil extends StatelessWidget {
             style: TextStyle(
               //color: Colors.black,
               //fontStyle: FontStyle.italic,
-              fontSize:16,
+              fontSize:18,
               //fontFamily: 'Times New Roman',
               //fontWeight: FontWeight.w100,
               ),
             ),
             Container(height:30),
             Container(
-              width:180,
+              width:220,
               height: 30,
               child:
               TextFormField(
                   textAlign: TextAlign.left,
                   textAlignVertical: TextAlignVertical.bottom,
+                  controller: myController,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Veuillez ne laisser pas le champ vide';
+                    }
+                    return null;
+                  },
                   decoration: InputDecoration(
-                  border: OutlineInputBorder(borderSide: BorderSide()),
-                  hintText: ('Entrez votre nom ici'),
-                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                ),
+                    border: OutlineInputBorder(borderSide: BorderSide()),
+                    hintText: ('Entrez votre nom ici'),
+                    hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                  ),
               ),
             ),
-            Container(height:10),
+            Container(height:15),
             Container(
               height: 30,
-              child:
-              RaisedButton(
-                textColor: Colors.black,
-                color: Colors.grey[75],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                  side: BorderSide(color: Colors.black12),
-                ),
-                child: 
-                  Text('Continuer', style:TextStyle(fontSize: 16)),
-                onPressed: () {
-                  naviguerPageUn(context);
-                }
-              ),
+              child: RaisedButton(
+                    textColor: Colors.black,
+                    color: Colors.grey[75],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      side: BorderSide(color: Colors.black12),
+                    ),
+                    child: 
+                      Text('Continuer', style:TextStyle(fontSize: 16)),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        texteTemp = myController.text;                      
+                        _naviguerPageUn(context);
+                        /*return showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              // Retrieve the text the user has entered by using the
+                              // TextEditingController.
+                              content: Text("Bienvenue, $texteTemp!"),
+                            );
+                          },
+                        );*/                                              
+                      }                  
+                    }
+              )
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.blue,
-              child: Text('Page deux'),
-              onPressed: () {
-                naviguerPageDeux(context);
-              },
-            ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.blue,
-              child: Text('Page Trois'),
-              onPressed: () {
-                naviguerPageTrois(context);
-              },
-            ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.blue,
-              child: Text('Page Adieu'),
-              onPressed: () {
-                naviguerPageAdieu(context);
-              },
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
+  void _naviguerPageUn(BuildContext context) {
+  String texteTemp = myController.text;
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PageUn(text: texteTemp),
+      ));
+  }
 }
-
 
 // Page Un
 
-
 class PageUn extends StatelessWidget {
-  Future naviguerPageDeux(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageDeux()));
-  }
+  final String text;
+  PageUn({Key key, @required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Page un'),
-        backgroundColor: Colors.redAccent,
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Suivante'),
-              onPressed: () {
-                Navigator.pop(context);
-                naviguerPageDeux(context);
-              },
+            Container(height:110),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                    child:                  
+                      Image.asset(
+                        'images/logo.jpg',
+                        width:310,
+                        height:60,
+                        fit:BoxFit.contain,
+                        //alignment: Alignment.center,
+                      ),
+                )
+              )
+              ]
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Retourner à la page d''accueil'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
+            Container(height: 17.5),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+                  child: 
+                  Text("Bienvenue $text!",
+                    style: TextStyle(fontSize: 16)
+                  ),
+              )
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+                  child: 
+                  Text("Pour accéder au campus aujourd'hui, tu dois compléter le questionnaire.",
+                    style: TextStyle(fontSize: 16)
+                  ),
+              )
+            ),
+            Container(height:15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Container(
+                  height: 25,
+                  width: 310,
+                  child:
+                  RaisedButton(
+                    textColor: Colors.black,
+                          color: Colors.grey[75],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            side: BorderSide(color: Colors.black12),
+                          ),
+                    child: Text('Completer le questionnaire',
+                            style: TextStyle(fontSize: 16)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _naviguerPageDeux(context);
+                    },
+                  )
+                ),
+              ]
+            ),
           ],
         ),
       ),
     );
+  }
+  void _naviguerPageDeux(BuildContext context) {
+  String texteTemp = text;
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PageDeux(text: texteTemp),
+      ));
   }
 }
 
@@ -175,106 +239,186 @@ class PageUn extends StatelessWidget {
 
 
 class PageDeux extends StatelessWidget {
-  Future naviguerPageTrois(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageTrois()));
-  }
+  final String text;
+  PageDeux({Key key, @required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Page deux'),
-        backgroundColor: Colors.redAccent,
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Completer le questionnaire'),
-              onPressed: () {
-                Navigator.pop(context);
-                naviguerPageTrois(context);
-              },
+            Container(height:50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                    child:                  
+                      Image.asset(
+                        'images/logo.jpg',
+                        width:310,
+                        height:60,
+                        fit:BoxFit.contain,
+                        //alignment: Alignment.center,
+                      ),
+                )
+              )
+              ]
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Retourner à la page d''accueil'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-//Page Trois
-
-
-class PageTrois extends StatelessWidget {
-  Future naviguerPageAdieu(context) async {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => PageAdieu()));
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page trois'),
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Soumettre'),
-              onPressed: () {
-                Navigator.pop(context);
-                naviguerPageAdieu(context);
-              },
+            Container(height: 17.5),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+                  child: 
+                  Text("Formulaire COVID-19 pour $text",
+                    style: TextStyle(fontSize: 16)
+                  ),
+              )
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Retourner à la page d''accueil'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            )
+            Container(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+                  child: 
+                  Text("Date 2020-11-25",
+                    style: TextStyle(fontSize: 16)
+                  ),
+              )
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                  child: 
+                  Text("Aujourd'hui, je me sens:",
+                    style: TextStyle(fontSize: 16)
+                  ),
+              )
+            ),
+            
+            //icônes
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FavoriteWidget(),
+                FavoriteWidget(),
+                FavoriteWidget(),
+              ],
+            ),
+            Row(
+              children: <Widget> [
+                Container(
+                  width: 190,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+                      child: 
+                      Text("J'ai voyagé hors du Canada dans les derniers 14 jours:",
+                        style: TextStyle(fontSize: 16)
+                      ),
+                  )
+                ),
+                FavoriteWidget(),
+                FavoriteWidget(),
+                Container()
+              ]
+            ),
+            Row(
+              children: <Widget> [
+                Container(
+                  width: 190,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+                      child: 
+                      Text("J'ai été en contacte avec un individu qui a eu COVID-19 dans les derniers 14 jours:",
+                        style: TextStyle(fontSize: 16)
+                      ),
+                  )
+                ),
+                FavoriteWidget(),
+                FavoriteWidget(),
+              ]
+            ),
+            Container(height:15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                Container(
+                  height: 25,
+                  width: 125,
+                  child:
+                  RaisedButton(
+                    textColor: Colors.black,
+                          color: Colors.grey[75],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            side: BorderSide(color: Colors.black12),
+                          ),
+                    child: Text('Soumettre',
+                            style: TextStyle(fontSize: 16)),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _naviguerPageUn(context);
+                    },
+                  )
+                ),
+              ]
+            ),
           ],
         ),
       ),
     );
   }
+  void _naviguerPageUn(BuildContext context) {
+  String texteTemp = text;
+  Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PageUn(text: texteTemp),
+      ));
+  }
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
 
-//Page Adieu
-
-
-class PageAdieu extends StatelessWidget {
-  
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Page adieu'),
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+      ],
+    );
+  }
+  void _toggleFavorite() {
+  setState(() {
+    if (_isFavorited) {
+      _favoriteCount -= 1;
+      _isFavorited = false;
+    } else {
+      _favoriteCount += 1;
+      _isFavorited = true;
+    }
+  });
+}
+}
+/*
             RaisedButton(
               textColor: Colors.white,
               color: Colors.redAccent,
@@ -282,10 +426,4 @@ class PageAdieu extends StatelessWidget {
               onPressed: () {
                 Navigator.pop(context);
               },
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+*/
